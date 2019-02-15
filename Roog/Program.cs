955238facff30b624b31578e4dea8d649f5cb176
stha <g17l3794@campu.ru.ac.za>
2019-02-15 09:49:23 -0800

    added student number to the output of Roog

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -6,7 +6,7 @@ namespace Roog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!" + "student no: g17l3794");
         }
     }
 }
